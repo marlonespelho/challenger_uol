847091789141e293ab805d24e0cab8ec1ea0eef5
Marlon <marlon.b.e.mbe@gmail.com>
2020-04-02 22:12:17 -0300

    Initial commit
    Configure ambient to develop

---
+++ src/main/kotlin/com/espelho/challenger/uol/ChallengerUolApplication.kt
@@ -0,0 +1,11 @@
+package com.espelho.challenger.uol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengerUolApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChallengerUolApplication>(*args)
+}
